174cea5ecb2fe48f27ccfc86f83a3126445b0b2c
ZhaoqianXue <zx136@georgetown.edu>
2025-10-21 18:57:35 -0400

    Remove unnecessary directory creation from frontend startup script
    - Frontend should not create backend directories
    - Directories are created by backend startup script only
    - Maintain proper separation of concerns

--- frontend_startup.sh
+++ frontend_startup.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# Create necessary directories
-mkdir -p /home/site/wwwroot/jobs
-mkdir -p /home/site/wwwroot/agent_uploads
-mkdir -p /home/site/wwwroot/temp_ranking_jobs
-
-# Set proper permissions
-chmod -R 755 /home/site/wwwroot/jobs
-chmod -R 755 /home/site/wwwroot/agent_uploads
-chmod -R 755 /home/site/wwwroot/temp_ranking_jobs
-
 # Start the NiceGUI frontend
 python code_app/frontend/main.py
